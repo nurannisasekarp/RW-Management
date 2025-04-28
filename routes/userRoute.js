e2cd9f7007404408cfd5a21ba801b7ac799c7eba
bintangmayra <bintangmayradestianti@smkwikrama.sch.id>
2025-04-28 11:10:13 +0700

    CRUD USER

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,8 +4,19 @@ const userController = require('../controllers/userController');
 
 console.log('Users routes loaded');
 
+// Route untuk export users ke Excel
 router.get('/export-users', userController.exportUsers);
+
+// Route untuk import users dari Excel
 router.post('/import-users', userController.uploadMiddleware, userController.importUsers);
+
+// Route untuk create user baru
 router.post('/', userController.createUsers);
+
+// Route untuk edit user berdasarkan ID
+router.put('/:id', userController.editUser);
+
+// Route untuk delete user berdasarkan ID
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
